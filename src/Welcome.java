3b68fba7e36c0e8f30dddec3453cf14b5acab33b
ChungKein4320 <trungkien7818@gmail.com>
2024-08-10 20:14:29 +0700

    first commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Cao Huynh Trung Kien");
+        System.out.println("Welcome to DHKTPM19A");
+    }
+}
